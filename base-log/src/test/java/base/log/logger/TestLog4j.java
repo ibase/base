f1fc45ce5445d72b1f81b892af71555575cf6683
pan <pan@gmail.com>
2016-04-06 09:57:11 +0800

    add base log - log4j & logback

---
+++ base-log/src/test/java/base/log/logger/TestLog4j.java
@@ -0,0 +1,20 @@
+package base.log.logger;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+/**
+ * Created by Doing on 2016/4/5.
+ */
+public class TestLog4j {
+
+    private static final Logger logger = Logger.getLogger(TestLog4j.class);
+
+    @Test
+    public void testLog4j(){
+        logger.debug("debug");
+        logger.info("------ info");
+        logger.warn("------ ------ warn");
+        logger.error("------ ------ ------ error");
+    }
+}
